6f708e45844d12e77b55e7bb15f4c94b2da7216b
Gavin Stirling <gavin@intelligentediting.com>
2023-02-22 09:54:47 +0000

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Home Page</h1>
+        <NuxtLink to="logout">Log Out</NuxtLink>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
